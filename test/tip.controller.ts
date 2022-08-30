c36b756d0dd686fa1bba1a56392aa4e8247545cd
marvel <ibironkemarv@gmail.com>
2022-08-30 01:52:09 +0100

    send bitcoin test

--- test/tip.controller.ts
+++ test/tip.controller.ts
@@ -2,6 +2,9 @@ import 'mocha';
 import { expect } from 'chai';
 import request from 'supertest';
 import Server from '../server';
+import * as sinon from 'sinon';
+import sendOnchain from './../server/api/services/sendOnchain.services';
+import { onChainSuccess } from './fixtures/bitnob';
 
 describe('Check testnet/signet bitcoin address', () => {
   it('success - valid signet/testnet address', async () => {
@@ -39,5 +42,62 @@ describe('Check testnet/signet bitcoin address', () => {
       .expect(200);
 
     expect(res.body.message).to.be.false;
+  });
+});
+
+describe('send bitcoin onchain', () => {
+  let createOnchainStub: any;
+  beforeEach(() => {
+    createOnchainStub = sinon
+      .stub(sendOnchain, 'sendBitcoin')
+      .returns(Promise.resolve(onChainSuccess));
+  });
+
+  afterEach(() => {
+    createOnchainStub.restore();
+  });
+
+  it('success - onchain request sent', async () => {
+    const res = await request(Server)
+      .post('/api/tip/onchain')
+      .send({
+        customerEmail: 'pelumi@gmail.com',
+        satoshis: 300,
+        address: 'tb1q9h0yjdupyfpxfjg24rpx755xrplvzd9hz2nj7v',
+      })
+      .expect(200);
+
+    expect(res.body.message).to.eql('Transaction successfully submitted');
+    expect(res.body.status).to.be.true;
+  });
+
+  it('failure- customerEmail not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/onchain')
+      .send({ satoshis: 400, address: 'tvsdsdffdfd' })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: customerEmail, satoshis, address'
+    );
+  });
+
+  it('failure- satoshis not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/onchain')
+      .send({ customerEmail: 'bamidele@gmail.com', address: 'terwrewere' })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: customerEmail, satoshis, address'
+    );
+  });
+
+  it('failure- address not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/onchain')
+      .send({ customerEmail: 'elect@gmail.com', satoshis: 500 })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: customerEmail, satoshis, address'
+    );
   });
 });
