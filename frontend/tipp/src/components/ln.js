33598de6a59dbad718c690c0f04646bc097113e8
marvel <ibironkemarv@gmail.com>
2022-08-30 16:54:40 +0100

    bootstrap exploration

--- frontend/tipp/src/components/ln.js
+++ frontend/tipp/src/components/ln.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 export const Ln = () => {
   return (
     <div>
-      <header className="App-header">
+      <header className="App-header-center">
+
         <h1>this is where ln stuff will take place</h1>
       </header>
     </div>
