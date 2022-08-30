46358f9f83c42660a611dbb56877ca0a8bd0af64
marvel <ibironkemarv@gmail.com>
2022-08-30 13:01:52 +0100

    omo ln test

--- test/fixtures/bitnob.ts
+++ test/fixtures/bitnob.ts
@@ -22,3 +22,47 @@ export const onChainSuccess = {
     customerId: '0cf31339-b4e2-41bd-850b-26f238169c62',
   },
 };
+
+export const initiateLnPaymentSuccess = {
+  status: true,
+  message: 'Transaction summary',
+  data: {
+    request:
+      'lntb1500n1p3sma3jpp50zk3822q5xpeluyp4jlwa6ua2y2egl6t6qrw63ptm98yqes2futsdp42fjkzep6ypp9gseqvejk2ernyp6x2mr9vaexzmfqvd5xzmnwv4kq6cqzpgxqr23ssp58ecprkj0nlvdt36wmvngzmrf4kx74kx6yajfnl3e80hn3rwk3mns9qyyssq4g456cttt7l79hw5hewz8fqz8wfcg2kldu6jf8gguh78c4ssq3dhqfnzcp9fl39e0hytxcfcmr8ca90rzxmtx6zhkzfxdrp3zmmqkpqqhntnga',
+    satFee: 20,
+    expiry: '2022-08-30T14:36:18.000Z',
+    satAmount: 150,
+    is_expired: false,
+    description: 'Read: BTC feeds telegram channel\r',
+    btcFee: 2e-7,
+    fee: 0,
+    btcAmount: 0.0000015,
+    amount: 0.03,
+  },
+};
+
+export const payLnInvoiceSuccess = {
+  status: true,
+  message: 'Transaction summary',
+  data: {
+    id: 'c7d74630-64b1-4e19-95c7-620fd414ae0b',
+    createdAt: '2022-08-30T11:38:02.676Z',
+    updatedAt: '2022-08-30T11:38:02.676Z',
+    reference: 'wwwwwwwwesnjsanasjas',
+    amount: 0.03,
+    fees: 0,
+    btcFees: 2e-7,
+    btcAmount: 0.0000015,
+    satFees: 20,
+    satAmount: 150,
+    spotPrice: 20375.85,
+    action: 'send_bitcoin',
+    type: 'debit',
+    status: 'pending',
+    channel: 'lightning',
+    paymentRequest:
+      'lntb1500n1p3sma3jpp50zk3822q5xpeluyp4jlwa6ua2y2egl6t6qrw63ptm98yqes2futsdp42fjkzep6ypp9gseqvejk2ernyp6x2mr9vaexzmfqvd5xzmnwv4kq6cqzpgxqr23ssp58ecprkj0nlvdt36wmvngzmrf4kx74kx6yajfnl3e80hn3rwk3mns9qyyssq4g456cttt7l79hw5hewz8fqz8wfcg2kldu6jf8gguh78c4ssq3dhqfnzcp9fl39e0hytxcfcmr8ca90rzxmtx6zhkzfxdrp3zmmqkpqqhntnga',
+    companyId: 'a1fd406c-48f5-4fcd-ac8c-7f8f3f911635',
+    customerId: '0cf31339-b4e2-41bd-850b-26f238169c62',
+  },
+};
--- test/tip.controller.ts
+++ test/tip.controller.ts
@@ -4,7 +4,12 @@ import request from 'supertest';
 import Server from '../server';
 import * as sinon from 'sinon';
 import sendOnchain from './../server/api/services/sendOnchain.services';
-import { onChainSuccess } from './fixtures/bitnob';
+import sendLightningService from '../server/api/services/sendLightning.services';
+import {
+  onChainSuccess,
+  payLnInvoiceSuccess,
+  initiateLnPaymentSuccess,
+} from './fixtures/bitnob';
 
 describe('Check testnet/signet bitcoin address', () => {
   it('success - valid signet/testnet address', async () => {
@@ -98,6 +103,99 @@ describe('send bitcoin onchain', () => {
       .expect(400);
     expect(res.body.message).to.eql(
       'The following parameters are all required for this route: customerEmail, satoshis, address'
+    );
+  });
+});
+
+describe('initiate ln payment', () => {
+  let createLnStub: any;
+  beforeEach(() => {
+    createLnStub = sinon
+      .stub(sendLightningService, 'check')
+      .returns(Promise.resolve(initiateLnPaymentSuccess));
+  });
+
+  afterEach(() => {
+    createLnStub.restore();
+  });
+
+  it('success - success initiate ln payment', async () => {
+    const res = await request(Server)
+      .post('/api/tip/ln')
+      .send({
+        request:
+          'lntb1500n1p3sma3jpp50zk3822q5xpeluyp4jlwa6ua2y2egl6t6qrw63ptm98yqes2futsdp42fjkzep6ypp9gseqvejk2ernyp6x2mr9vaexzmfqvd5xzmnwv4kq6cqzpgxqr23ssp58ecprkj0nlvdt36wmvngzmrf4kx74kx6yajfnl3e80hn3rwk3mns9qyyssq4g456cttt7l79hw5hewz8fqz8wfcg2kldu6jf8gguh78c4ssq3dhqfnzcp9fl39e0hytxcfcmr8ca90rzxmtx6zhkzfxdrp3zmmqkpqqhntnga',
+      })
+      .expect(200);
+    expect(res.body.message.message).to.eql('Transaction summary');
+    expect(res.body.message.status).to.be.true;
+  });
+
+  it('failure- request not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/ln')
+      .send({ address: 'tvsdsdffdfd' })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: request'
+    );
+  });
+});
+
+describe('pay ln invoice ', () => {
+  let createLnStub: any;
+  beforeEach(() => {
+    createLnStub = sinon
+      .stub(sendLightningService, 'payInvoice')
+      .returns(Promise.resolve(payLnInvoiceSuccess));
+  });
+
+  afterEach(() => {
+    createLnStub.restore();
+  });
+
+  it('success - success initiate ln payment', async () => {
+    const res = await request(Server)
+      .post('/api/tip/ln/pay')
+      .send({
+        reference: 'tvsdsdffdfd',
+        customerEmail: 'mm@gmail.com',
+        request:
+          'lntb1500n1p3sma3jpp50zk3822q5xpeluyp4jlwa6ua2y2egl6t6qrw63ptm98yqes2futsdp42fjkzep6ypp9gseqvejk2ernyp6x2mr9vaexzmfqvd5xzmnwv4kq6cqzpgxqr23ssp58ecprkj0nlvdt36wmvngzmrf4kx74kx6yajfnl3e80hn3rwk3mns9qyyssq4g456cttt7l79hw5hewz8fqz8wfcg2kldu6jf8gguh78c4ssq3dhqfnzcp9fl39e0hytxcfcmr8ca90rzxmtx6zhkzfxdrp3zmmqkpqqhntnga',
+      })
+      .expect(200);
+    console.log(res.body.message);
+    expect(res.body.message.message).to.eql('Transaction summary');
+    expect(res.body.message.status).to.be.true;
+  });
+
+  it('failure- request not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/ln/pay')
+      .send({ reference: 'tvsdsdffdfd', customerEmail: 'mm@gmail.com' })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: request, reference, customerEmail'
+    );
+  });
+
+  it('failure- reference not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/ln/pay')
+      .send({ address: 'tvsdsdffdfd', reference: 'wedfgfjhtredfver' })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: request, reference, customerEmail'
+    );
+  });
+
+  it('failure- customerEmail not included in request body ', async () => {
+    const res = await request(Server)
+      .post('/api/tip/ln/pay')
+      .send({ address: 'tvsdsdffdfd', reference: 'erefeghytrew' })
+      .expect(400);
+    expect(res.body.message).to.eql(
+      'The following parameters are all required for this route: request, reference, customerEmail'
     );
   });
 });
